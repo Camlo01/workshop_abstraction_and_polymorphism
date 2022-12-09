6074d4a78efa6f7baa3c6b06ffc4cd93999f9e2a
camlo01 <camilobeltran174@gmail.com>
2022-12-09 15:01:39 -0500

    improved documentation on showMenu() | added line breaks in clearView()

--- src/main/java/ui/UiHome.java
+++ src/main/java/ui/UiHome.java
@@ -17,38 +17,43 @@ public class UiHome {
         // Show all prompts to select what you want to do
         menuBody();
         do {
-
             Scanner inputOption = new Scanner(System.in);
             response = Integer.parseInt(inputOption.next());
 
             switch (response) {
                 case 1:
+//                    1. Ship menu - [here you will find more options]
                     clearView();
                     UiShipMenu.shipMenu();
                     response = 0;
                     break;
                 case 2:
+//                    2. See quickly all the ships - [Consult database]
                     UiShipMenu.viewShips();
                     System.out.println("5. To see the menu again");
                     System.out.print("Option: ");
                     break;
                 case 3:
+//                    3. Application information - version
                     clearView();
                     System.out.println("5. To see the menu again");
-                    System.out.println("Version: 1.0");
+                    System.out.println("Version: 2.0");
                     System.out.println("Creator: Camilo Beltrán");
                     System.out.println("Support: support@camilo.com");
                     break;
                 case 4:
+//                    4. Close application - [X]
                     clearView();
                     System.out.println("Program closed, come back soon [zzz]");
                     response = 0;
                     break;
                 case 5:
+//                    5. To see the menu again
                     clearView();
                     menuBody();
                     break;
                 default:
+//                    something wrong
                     clearView();
                     System.out.println("5. To see the menu again");
                     System.out.println("Try to enter a correct option [?]");
@@ -65,8 +70,8 @@ public class UiHome {
         System.out.println("Welcome to the aeronautics space agency app!");
         System.out.println("--- SELECT BY TYPING THE OPTION");
         System.out.println();
-        System.out.println("1. Create a ship - [here you will find more options] ");
-        System.out.println("2. See ships - [Consult database]");
+        System.out.println("1. Ship menu - [here you will find more options] ");
+        System.out.println("2. See quickly all the ships - [Consult database]");
         System.out.println("3. Application information - version");
         System.out.println("4. Close application - [X]");
         System.out.println();
@@ -78,6 +83,16 @@ public class UiHome {
      * Method with several line breaks to clean the console
      */
     protected static void clearView() {
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
         System.out.println();
         System.out.println();
         System.out.println();
