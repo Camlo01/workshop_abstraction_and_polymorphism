404f5e71a48e1ebdc56551db3aac67a85c3917bc
camlo01 <camilobeltran174@gmail.com>
2022-12-08 20:27:31 -0500

    added 4 new Spacecraft Objects to create automatically

--- src/main/java/dataBase/DataBaseShip.java
+++ src/main/java/dataBase/DataBaseShip.java
@@ -30,7 +30,6 @@ public class DataBaseShip {
 
     public static void showShips() {
         try {
-
             ships.forEach((ship) -> {
                 System.out.println(ship.getShipInfoInline());
             });
@@ -46,22 +45,29 @@ public class DataBaseShip {
      */
     public static void saveElementsAutomatic() {
         //Space Shuttle
-        Spacecraft s1 = new SpaceShuttle("Saturno V", 1967, 1972, "EEUU", "Eu Ship");
+        Spacecraft s1 = new SpaceShuttle("Saturno V", 1967, 1972, "American", "Eu Ship");
         Spacecraft s2 = new SpaceShuttle("Protón", 2001, "Russia", "Russian Ship");
         Spacecraft s3 = new SpaceShuttle("H-IIA", 2001, "Japan", "Japanese Ship");
 
 
         //Unmanned ship
-        Spacecraft s4 = new UnmannedShip("Explorer", 1958, 1970, "EEUU", "From NASA");
+        Spacecraft s4 = new UnmannedShip("Explorer", 1958, 1970, "American", "From NASA");
         Spacecraft s5 = new UnmannedShip("Luna I", 1959, "Russian", "From Russia");
         Spacecraft s6 = new UnmannedShip("Soho", 1995, "Europe", "Europe ESA/A SI");
 
 
         //Manned ship
 
-        Spacecraft s7 = new MannedShip("Apolo", 1966, 1975, "EEUU", "Eu Ship");
+        Spacecraft s7 = new MannedShip("Apollo", 1966, 1975, "American", "Eu Ship");
         Spacecraft s8 = new MannedShip("Soyuz", 1967, "Russia", "Soviet union");
         Spacecraft s9 = new MannedShip("EEI", 1998, "International", "For everyone");
+
+        //Capsule
+
+        Spacecraft s10 = new Capsule("Vostok", 1961, 1963, "Soviet Union", "first crewed space capsule");
+        Spacecraft s11 = new Capsule("Vostok", 1964, 1965, "Soviet Union", " had a backup solid fuel retrorocket ");
+        Spacecraft s12 = new Capsule("Apollo", 1975, 1975, "American", "first crewed space capsule");
+
 
         ships.add(s1);
         ships.add(s2);
@@ -72,6 +78,9 @@ public class DataBaseShip {
         ships.add(s7);
         ships.add(s8);
         ships.add(s9);
+        ships.add(s10);
+        ships.add(s11);
+        ships.add(s12);
 
     }
 
