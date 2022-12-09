70b354aecdac5d055a075fd6a48be505323c216b
camlo01 <camilobeltran174@gmail.com>
2022-12-09 11:26:30 -0500

    documented code

--- src/main/java/model/Ship.java
+++ src/main/java/model/Ship.java
@@ -5,16 +5,47 @@ package model;
  */
 public interface Ship {
 
+    /**
+     * Configure the year in which the ship finished the activity
+     *
+     * @param endingYear to set
+     */
     void shipEndsActivity(Integer endingYear);
 
-    void takeOffShip();
+    /**
+     * Ship releases the capsule
+     */
+    void detachCapsule();
 
+    /**
+     * Attach the capsule to the shipment
+     */
+    void attachCapsule();
+
+    /**
+     * Accelerate speed to one unit
+     */
     void accelerateSpeed();
 
+    /**
+     * Amount to increase ship speed
+     *
+     * @param speed amount
+     */
     void accelerateSpeed(int speed);
 
+    /**
+     * Complete and detailed information about the ship
+     *
+     * @return String with all details
+     */
     String getShipInfo();
 
+    /**
+     * Most important information in a single line with the type of ship
+     *
+     * @return single line text
+     */
     String getShipInfoInline();
 
 
